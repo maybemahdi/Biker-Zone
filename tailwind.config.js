75fa1efbcfd039f3fba5d6749202861ca87b3e39
maybemahdi <mh7266391@gmail.com>
2024-01-19 19:40:45 +0600

    Completed The Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  //...
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+}
